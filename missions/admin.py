ad30d69fd2aa719e3365a10276cb6b052ca92103
wmufunde <wmmufunde@yahoocom>
2017-02-18 23:09:50 +0000

    missions registered status

--- missions/admin.py
+++ missions/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Customer, Mission, Report
+from .models import Customer, Mission, Report, Status
 
 admin.site.register(Customer)
 admin.site.register(Mission)
 admin.site.register(Report)
+admin.site.register(Status)
